37accefa54e87582ca3af27b88726ed8b710695b
LuapRebel <paulleber23@gmail.com>
2017-01-30 09:40:00 -0800

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -25,9 +26,28 @@ app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
 		res.send({todos});
 	}, (e) => {
-		console.log(e);
+		res.status(400).send(e);
 	});
 });
+
+// GET /todos/:id
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
