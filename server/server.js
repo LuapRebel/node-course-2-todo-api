2498155e5583843037cbc9656aec18374108a46d
LuapRebel <paulleber23@gmail.com>
2017-02-03 18:53:19 -0800

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -118,6 +118,15 @@ app.delete('/todos/:id', (req, res) => {
 	});
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
+	});
+});
+
+
 //----------------------- PATCH ROUTES -------------------------
 // PATCH /todos/:id
 app.patch('/todos/:id', (req, res) => {
